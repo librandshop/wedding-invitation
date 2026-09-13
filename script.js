d55b4694399c2eb33c7f56000eb549f4e8247a93
librandshop <librandshop19@gmail.com>
2026-09-13 21:38:58 +0700

    Enable published RSVP form

--- script.js
+++ script.js
@@ -41,9 +41,9 @@ const translations = {
     seconds: "Seconds",
     rsvpEyebrow: "Kindly reply",
     rsvpTitle: "Will you celebrate with us?",
-    rsvpCopy: "Our RSVP form will be available here soon.",
-    rsvpButton: "RSVP link coming soon",
-    rsvpNote: "Please reply by the date shown on your invitation",
+    rsvpCopy: "Please let us know whether you can join us by completing our RSVP form.",
+    rsvpButton: "RSVP now",
+    rsvpNote: "Please reply by 30 September 2026",
     footerDate: "November 8, 2026",
   },
   th: {
@@ -78,9 +78,9 @@ const translations = {
     seconds: "\u0e27\u0e34\u0e19\u0e32\u0e17\u0e35",
     rsvpEyebrow: "\u0e42\u0e1b\u0e23\u0e14\u0e15\u0e2d\u0e1a\u0e23\u0e31\u0e1a",
     rsvpTitle: "\u0e04\u0e38\u0e13\u0e08\u0e30\u0e23\u0e48\u0e27\u0e21\u0e09\u0e25\u0e2d\u0e07\u0e01\u0e31\u0e1a\u0e40\u0e23\u0e32\u0e44\u0e2b\u0e21",
-    rsvpCopy: "\u0e41\u0e1a\u0e1a\u0e15\u0e2d\u0e1a\u0e23\u0e31\u0e1a\u0e08\u0e30\u0e1e\u0e23\u0e49\u0e2d\u0e21\u0e43\u0e0a\u0e49\u0e07\u0e32\u0e19\u0e17\u0e35\u0e48\u0e19\u0e35\u0e48\u0e40\u0e23\u0e47\u0e27 \u0e46 \u0e19\u0e35\u0e49",
-    rsvpButton: "\u0e25\u0e34\u0e07\u0e01\u0e4c RSVP \u0e40\u0e23\u0e47\u0e27 \u0e46 \u0e19\u0e35\u0e49",
-    rsvpNote: "\u0e01\u0e23\u0e38\u0e13\u0e32\u0e15\u0e2d\u0e1a\u0e23\u0e31\u0e1a\u0e20\u0e32\u0e22\u0e43\u0e19\u0e27\u0e31\u0e19\u0e17\u0e35\u0e48\u0e23\u0e30\u0e1a\u0e38\u0e43\u0e19\u0e1a\u0e31\u0e15\u0e23\u0e40\u0e0a\u0e34\u0e0d",
+    rsvpCopy: "\u0e01\u0e23\u0e38\u0e13\u0e32\u0e01\u0e23\u0e2d\u0e01\u0e41\u0e1a\u0e1a\u0e15\u0e2d\u0e1a\u0e23\u0e31\u0e1a\u0e40\u0e1e\u0e37\u0e48\u0e2d\u0e41\u0e08\u0e49\u0e07\u0e40\u0e23\u0e32\u0e27\u0e48\u0e32\u0e04\u0e38\u0e13\u0e08\u0e30\u0e21\u0e32\u0e23\u0e48\u0e27\u0e21\u0e07\u0e32\u0e19\u0e44\u0e14\u0e49\u0e2b\u0e23\u0e37\u0e2d\u0e44\u0e21\u0e48",
+    rsvpButton: "\u0e15\u0e2d\u0e1a\u0e23\u0e31\u0e1a RSVP",
+    rsvpNote: "\u0e01\u0e23\u0e38\u0e13\u0e32\u0e15\u0e2d\u0e1a\u0e23\u0e31\u0e1a\u0e20\u0e32\u0e22\u0e43\u0e19\u0e27\u0e31\u0e19\u0e17\u0e35\u0e48 30 \u0e01\u0e31\u0e19\u0e22\u0e32\u0e22\u0e19 \u0e1e.\u0e28. 2569",
     footerDate: "8 \u0e1e\u0e24\u0e28\u0e08\u0e34\u0e01\u0e32\u0e22\u0e19 \u0e1e.\u0e28. 2569",
   },
 };
@@ -138,4 +138,3 @@ try {
 
 updateCountdown();
 window.setInterval(updateCountdown, 1000);
-
